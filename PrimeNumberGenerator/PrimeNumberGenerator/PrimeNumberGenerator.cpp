e2c2c664c479092d5e63d658df88e3be99f4b809
Y1nfinity <66399878+Y1nfinity@users.noreply.github.com>
2020-07-08 10:21:17 -0500

    Add files via upload

---
+++ PrimeNumberGenerator/PrimeNumberGenerator/PrimeNumberGenerator.cpp
@@ -0,0 +1,50 @@
+// PrimeNumberGenerator.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+int main()
+{
+    int low, high, i, flag;
+
+    cout << "Enter two numbers(intervals): ";
+    cin >> low >> high;
+
+    ofstream myfile("Prime_Number_Generator.txt");
+
+    if (myfile.is_open())
+    {
+        myfile << "Prime numbers between " << low << " and " << high << " are: \n";
+        while (low < high)
+        {
+            flag = 0;
+
+            for (i = 2; i <= low / 2; ++i)
+            {
+                if (low % i == 0)
+                {
+                    flag = 1;
+                    break;
+                }
+            }
+
+            if (flag == 0)
+                myfile << low << " ";
+
+            ++low;
+        }
+
+        myfile << "\nPress x to exit the file. \n";
+        myfile.close();
+    }
+    else cout << "Unable to open file";
+
+    
+
+
+
+    return 0;
+}
